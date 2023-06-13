5bd19179cac7857bf0c37086866fce2a38e665df
Fabio Batista <fabiobatista04@hotmail.com>
2023-06-13 12:33:42 -0300

    fix constructor

--- src/main/java/br/com/luizalabs/customerservice/impl/ProductImpl.java
+++ src/main/java/br/com/luizalabs/customerservice/impl/ProductImpl.java
@@ -1,10 +1,12 @@
 package br.com.luizalabs.customerservice.impl;
 
 import br.com.luizalabs.customerservice.impl.model.ProductPageImpl;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
+@AllArgsConstructor
 public class ProductImpl {
 
     private FacadeImpl facade;
