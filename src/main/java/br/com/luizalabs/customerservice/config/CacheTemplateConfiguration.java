baf8102ed3fb7edba9cd63e95167b4dcfabb136a
Fabio Batista <fabiobatista04@hotmail.com>
2023-06-14 22:13:42 -0300

    refactor cache template

---
+++ src/main/java/br/com/luizalabs/customerservice/config/CacheTemplateConfiguration.java
@@ -0,0 +1,48 @@
+package br.com.luizalabs.customerservice.config;
+import br.com.luizalabs.customerservice.impl.model.CustomerImplModel;
+import br.com.luizalabs.customerservice.impl.model.ProductImplModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisConfiguration;
+import org.springframework.data.redis.core.ReactiveRedisOperations;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@Import(RedisConfiguration.class)
+public class CacheTemplateConfiguration {
+
+    @Bean
+    public ReactiveRedisOperations<String, ProductImplModel> redisOperationsProduct(
+            ReactiveRedisConnectionFactory factory
+    ) {
+        Jackson2JsonRedisSerializer<ProductImplModel> serializer =
+                new Jackson2JsonRedisSerializer<>(ProductImplModel.class);
+
+        RedisSerializationContext.RedisSerializationContextBuilder<String, ProductImplModel> builder =
+                RedisSerializationContext.newSerializationContext(new StringRedisSerializer());
+
+        RedisSerializationContext<String, ProductImplModel> context = builder.value(serializer).build();
+
+        return new ReactiveRedisTemplate<>(factory, context);
+    }
+
+    @Bean
+    public ReactiveRedisOperations<String, CustomerImplModel> redisOperationsCustomer(
+            ReactiveRedisConnectionFactory factory
+    ) {
+        Jackson2JsonRedisSerializer<CustomerImplModel> serializer =
+                new Jackson2JsonRedisSerializer<>(CustomerImplModel.class);
+
+        RedisSerializationContext.RedisSerializationContextBuilder<String, CustomerImplModel> builder =
+                RedisSerializationContext.newSerializationContext(new StringRedisSerializer());
+
+        RedisSerializationContext<String, CustomerImplModel> context = builder.value(serializer).build();
+
+        return new ReactiveRedisTemplate<>(factory, context);
+    }
+}
