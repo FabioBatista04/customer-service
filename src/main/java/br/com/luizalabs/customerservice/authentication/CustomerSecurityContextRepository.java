448f2a69a9305e85dd6f14ffdb9b97254bd4d494
Fabio Batista <fabiobatista04@hotmail.com>
2023-06-16 21:12:56 -0300

    refactor indentation

--- src/main/java/br/com/luizalabs/customerservice/authentication/CustomerSecurityContextRepository.java
+++ src/main/java/br/com/luizalabs/customerservice/authentication/CustomerSecurityContextRepository.java
@@ -1,6 +1,7 @@
 package br.com.luizalabs.customerservice.authentication;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+@Log4j2
 @Component
 @AllArgsConstructor
 public class CustomerSecurityContextRepository implements ServerSecurityContextRepository {
@@ -24,11 +26,11 @@ public class CustomerSecurityContextRepository implements ServerSecurityContextR
 
     public Mono<SecurityContext> load(ServerWebExchange swe) {
         return Mono.justOrEmpty(swe.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION))
-            .filter(authHeader -> authHeader.startsWith("Bearer "))
-            .flatMap(authHeader -> {
-                String authToken = authHeader.substring(7);
-                Authentication auth = new UsernamePasswordAuthenticationToken(authToken, authToken);
-                return customerAuthenticationManager.authenticate(auth).map(SecurityContextImpl::new);
-            });
+                .filter(authHeader -> authHeader.startsWith("Bearer "))
+                .flatMap(authHeader -> {
+                    String authToken = authHeader.substring(7);
+                    Authentication auth = new UsernamePasswordAuthenticationToken(authToken, authToken);
+                    return customerAuthenticationManager.authenticate(auth).map(SecurityContextImpl::new);
+                });
     }
 }
--- src/main/java/br/com/luizalabs/customerservice/exeptions/GenericException.java
+++ src/main/java/br/com/luizalabs/customerservice/exeptions/GenericException.java
@@ -4,15 +4,14 @@ import br.com.luizalabs.customerservice.impl.model.ErrorEnum;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.HashMap;
 import java.util.Map;
 @Getter
 @Setter
 public class GenericException extends RuntimeException{
     private final ErrorEnum errorEnum;
-    private final Map<String,String> fields;
+    private final Map<String, String> fields;
 
-    public GenericException(ErrorEnum errorEnum, String message, Map<String,String> fields) {
+    public GenericException(ErrorEnum errorEnum, String message, Map<String, String> fields) {
         super(message);
         this.errorEnum = errorEnum;
         this.fields = fields;
